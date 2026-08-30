1c529a7391149e803b684881508fa7216baca91f
youpyxx <ermolaevads08@gmail.com>
2026-08-30 15:31:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kabinet-v1';
+const CACHE = 'kabinet-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
